346c84f290f145003c003225a3032b15e20d3b0d
Ryan England <me@rengland.org>
2023-11-07 23:42:21 -0800

    Added handlebars for homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const { BlogPost, User } = require("../models");
+
+// Send all blog data to homepage
+router.get("/", async (req, res) => {
+  try {
+    const blogData = await BlogPost.findAll({
+      include: [{ model: User, attributes: ["username"] }],
+    });
+
+    const posts = blogData.map((blogPosts) => blogPosts.get({ plain: true }));
+
+    res.render("homepage", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch {
+    res.render("500");
+  }
+});
