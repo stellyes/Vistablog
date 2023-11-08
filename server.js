43948d77fc4636742934c457602a5ec2604e2c56
Ryan England <me@rengland.org>
2023-11-07 22:41:35 -0800

    Added database connection now that secure password storing is enabled

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 // Set up express instance and port
 const app = express();
@@ -9,6 +11,22 @@ const PORT = process.env.PORT || 3001;
 // Apply necessary middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Connect to our database
+const sess = {
+  secret: "VistaBlog",
+  cookie: {
+    // Two weeks before login expires
+    maxAge: 1209600000,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 // Attach routes to server
 app.use(routes);
